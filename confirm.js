8d076eb7668ec3917726163082324a4401b249d0
Satyam Kumar <56245953+satya4498@users.noreply.github.com>
2022-11-20 15:03:18 +0530

    Update confirm.js
    Final

--- confirm.js
+++ confirm.js
@@ -1,28 +1,45 @@
-let nameEl = document.getElementById("name");
-let Name = nameEl.value.trim().split(" ");
+let nameEl = sessionStorage.getItem("name");
+console.log(nameEl);
+let Name = nameEl.split(" ");
 let fName = document.getElementById("firstName");
 fName.textContent = Name[0];
 
-let emailEl = document.getElementById("email");
+let emailEl = sessionStorage.getItem("email");
 let mail = document.getElementById("userEmail");
- mail.textContent = emailEl.value;
+ mail.textContent = emailEl;
 
 let randomCode = Math.floor(Math.random() * 10000);
 console.log(randomCode);
 let r = window.location.search;
 let btnEl = document.getElementById("button");
+let formEl = document.getElementById("confirmForm");
 
 btnEl.addEventListener("click",function(){
     let errMsg = document.getElementById("otpErr");
     errMsg.textContent = r;
     let otpInput = document.getElementById("otp");
-    let amtValidationEl = document.getElementById("validation");
+    let otpValidationEl = document.getElementById("validation");
     let val = parseInt(otpInput.value);
+    let container = document.getElementById("otpContainer");
+    let otpCount = 1;
     if(val===randomCode){
+        container.removeChild(otpValidationEl);
         errMsg.textContent = "Validation Successfull"
+        errMsg.style.color = "green";
+        container.appendChild(errMsg);
+        formEl.action = "http://pixel6.co/";
+
+    }
+    else if(val!==randomCode && otpCount<=3){ 
+
+        errMsg.textContent="Please Enter the correct otp";
+        otpCount++;
     }
     else{
-        errMsg.textContent="Validation Failed"
+        container.removeChild(otpValidationEl);
+        errMsg.textContent = "Validation Failed";
+        container.appendChild(errMsg);
+       
     }
     
 });
